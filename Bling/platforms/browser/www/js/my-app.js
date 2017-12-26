383891b33e613726dfbcfc2a16306cfefbf1182d
Nitin Kumar <nitinkumar31@yahoo.com>
2017-12-26 21:54:52 +0530

    Add navigation drawer

--- Bling/platforms/browser/www/js/my-app.js
+++ Bling/platforms/browser/www/js/my-app.js
@@ -27,6 +27,28 @@ var myApp = new Framework7({
     template7Pages: true
 });
 
+function Application(){
+  var myApp = new Framework7({
+    swipePanel : 'left'
+  });
+
+  var mainView = myApp.addView('.view-main');
+  $$('#toggleID').on('click',function()
+  {
+    myApp.openPanel('left');
+  });
+
+  $$('#prefID').on('click',function(e)
+  {
+    myApp.closePanel();
+    mainView.router.loadPage('pages/prefer.html');
+  });
+
+}
+
+Application();
+
+
 // Init View
 var mainView = myApp.addView('.view-main', {
     // Don't worry about that Material doesn't support it
@@ -103,6 +125,7 @@ $$(document).on('pageInit', '.page[data-page="register"]', function (e) {
               {
                 localStorage.id = id;
                 myApp.alert("Registration successfull");
+                mainView.router.loadPage('received-message.html');
               }
 
               else if(data=="failed")
