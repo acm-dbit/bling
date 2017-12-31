a21a753a08d1aa86b0c24e65d66b67adfffd9c37
Jaffrey Joy <jaffreyjoy@gmail.com>
2017-12-31 14:13:10 +0530

    single msg view for sent and received messages done TEST : OK

--- Bling/platforms/android/assets/www/js/my-app.js
+++ Bling/platforms/android/assets/www/js/my-app.js
@@ -267,7 +267,7 @@ $$(document).on('pageInit', '.page[data-page="camera"]', function (e) {
     $$('#cam_btn').on('click', function () {
 
         navigator.camera.getPicture(function(result){
-          
+
           $$("#image").attr("src",result);
         },
 
@@ -357,7 +357,7 @@ function newUserType(){
       // myApp.alert(type+localStorage.department+localStorage.year);
       // myApp.alert(data);
 
-      if (data.length > 10) {
+      if (data.length > 50) {
         res = JSON.parse(data);
         insertMsgData(res);
         getNDisplayMsgData();
@@ -390,10 +390,9 @@ function oldUserType(resultSet) {
         res = JSON.parse(data);
         insertMsgData(res);
       }
-      getNDisplayMsgData();
-
     }
   });
+  getNDisplayMsgData();
 }
 
 $$(document).on("pageInit", '.page[data-page="received-message"]', function(e) {
@@ -426,6 +425,12 @@ $$(document).on("pageInit", '.page[data-page="received-message"]', function(e) {
       myApp.alert('SELECT error (user type): ' + error.message);
     }
   );
+
+  $$(document).on("click", "li.msg", function () {
+    var id = $$(this).attr('id');
+    localStorage.clicked_msg_id = id;
+    mainView.router.loadPage('view-received-message.html');
+  });
 
 });
 
@@ -543,6 +548,107 @@ $$(document).on("pageInit", '.page[data-page="sent-message"]', function (e) {
       // myApp.alert("old");
       getNDisplaySentMsgData();
     }
+  },
+    function (error) {
+      myApp.alert('SELECT error (user type): ' + error.message);
+    }
+  );
+
+  $$(document).on("click", "li.msg", function () {
+    var id = $$(this).attr('id');
+    localStorage.clicked_msg_id = id;
+    mainView.router.loadPage('view-sent-message.html');
+  });
+
+});
+
+$$(document).on("pageInit", '.page[data-page="view-sent-message"]', function (e) {
+  // myApp.alert('here');
+  var query = "SELECT * FROM msg_data WHERE msg_id = ?";
+
+  db.executeSql(query, [localStorage.clicked_msg_id], function (resultSet) {
+
+    // myApp.alert('here in sql execute');
+    var dept_year = resultSet.rows.item(0).department + "  (" + resultSet.rows.item(0).year + ")";
+    var msg_date = resultSet.rows.item(0).date;
+    var msg_time = resultSet.rows.item(0).time;
+    var msg_subject = resultSet.rows.item(0).subject;
+    var message_content = resultSet.rows.item(0).message;
+
+
+    //Dynamic html for single msg view
+    msg_html =
+      '<div id="'+localStorage.clicked_msg_id+'" class="card">'+
+          '<div class="card-header">'+dept_year+'</div>'+
+          '<div class="card-content">'+
+              '<div class="list-block media-list">'+
+                  '<ul>'+
+                      '<li class="item-content">'+
+                          '<div class="item-inner">'+
+                              '<div class="item-title-row">'+
+                                  '<div class="item-title"> <b>Subject</b> : <br>'+msg_subject+'</div>'+
+                              '</div><br>'+
+                              '<b>Message</b> : <br>'+message_content+'<br><br>'+
+                          '</div>'+
+                      '</li>'+
+                ' </ul>'+
+              '</div>'+
+          '</div>'+
+          '<div class="card-footer">'+
+              '<span>'+msg_date+'</span>'+
+              '<span>'+msg_time+'</span>'+
+          '</div>'+
+      '</div>';
+    // alert(msg_html);
+    //Inserting generated html from above into parent html element i.e <div> with id #view_message
+    $$("#view_message").html(msg_html);
+  },
+    function (error) {
+      myApp.alert('SELECT error (user type): ' + error.message);
+    }
+  );
+});
+
+
+$$(document).on("pageInit", '.page[data-page="view-received-message"]', function (e) {
+
+  var query = "SELECT * FROM msg_data WHERE msg_id = ?";
+
+  db.executeSql(query, [localStorage.clicked_msg_id], function (resultSet) {
+    var msg_fac_name = "Prof. " + resultSet.rows.item(0).fac_name;
+    var msg_date = resultSet.rows.item(0).date;
+    var msg_time = resultSet.rows.item(0).time;
+    var msg_subject = resultSet.rows.item(0).subject;
+    var message_content = resultSet.rows.item(0).message;
+
+    myApp.alert(message_content);
+
+    //Dynamic html for single msg view
+    msg_html =
+      '<div id="'+localStorage.clicked_msg_id+'" class="card">'+
+          '<div class="card-header">'+msg_fac_name+'</div>'+
+          '<div class="card-content">'+
+              '<div class="list-block media-list">'+
+                  '<ul>'+
+                      '<li class="item-content">'+
+                          '<div class="item-inner">'+
+                              '<div class="item-title-row">'+
+                                  '<div class="item-title"> <b>Subject</b> : <br>'+msg_subject+'</div>'+
+                                  '</div><br>'+
+                              '<b>Message</b> : <br>'+message_content+'<br><br>'+
+                          '</div>'+
+                      '</li>'+
+                ' </ul>'+
+              '</div>'+
+          '</div>'+
+          '<div class="card-footer">'+
+              '<span>'+msg_date+'</span>'+
+              '<span>'+msg_time+'</span>'+
+          '</div>'+
+      '</div>';
+
+    //Inserting generated html from above into parent html element i.e <div> with id #view_message
+    $$("#view_message").html(msg_html);
   },
     function (error) {
       myApp.alert('SELECT error (user type): ' + error.message);
