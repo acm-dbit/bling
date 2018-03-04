45140d72d5e4f1185f2c2b496ea5a82b7747d936
Jaffrey Joy <jaffreyjoy@gmail.com>
2018-03-04 15:10:46 +0530

    push notifications in backend ENABLED

--- Backend/message.php
+++ Backend/message.php
@@ -1,6 +1,6 @@
 <?php
 	header("Access-Control-Allow-Origin: *");
-	// include 'notif.php';
+	include 'notif.php';
 	$conn = new mysqli("localhost","id4053307_blingdb","/blingdb/","id4053307_blingdb");
 
 	if($conn->connect_error)
@@ -19,37 +19,17 @@
 
 	$insert = "INSERT INTO msg_data (id,date,time,fac_name,subject,message,department,year) VALUES('$id','$date','$time','$fac_name','$subject','$message','$department','$year')";
 
-	// $get_name = "SELECT name FROM stud_data WHERE id='$id'";
 	$result = $conn->query($insert);
 
 	$res_type = "success";
-
-	// if($conn->query($insert)){
-
-	//     $result = $conn->query($get_name);
-
-	// 	if($result-> num_rows > 0){
-	// 		while($row = $result->fetch_assoc()){
-    // 	    	$name = $row['name'];
-    //     	}
-
-    //     	send_notif($name,$department,$year);
-
-	// 	}
-
-	// 	$res_type =  "success";
-	// }
-
-	// else
-	// 	$res_type = "failed";
-
-
 
 	$get_msg_id = "SELECT * FROM msg_data WHERE id='$id' AND date='$date' AND time='$time'";
 	$result = $conn->query($get_msg_id);
 	$row = $result->fetch_assoc();
 	$msg_id = $row['msg_id'];
 	$to_send = '{"res_type":"'.$res_type.'","msg_id":"'.$msg_id.'","date":"'.$date.'","time":"'.$time.'"}';
+
+	send_notif($fac_name,$department,$year);
 
 	echo $to_send;
 
